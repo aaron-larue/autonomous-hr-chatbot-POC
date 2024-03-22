e9e032dfc64d7b1c5b9820a07fbd154c8787cfca
aaron-larue <45748097+aaron-larue@users.noreply.github.com>
2024-03-22 22:35:05 +0000

    Adding replit files for easy project config

---
+++ replit.nix
@@ -0,0 +1,18 @@
+{pkgs}: {
+  deps = [
+    pkgs.zeromq
+    pkgs.libffi
+    pkgs.gitFull
+    pkgs.gdb
+    pkgs.arrow-cpp
+    pkgs.cacert
+    pkgs.pkg-config
+    pkgs.openssl
+    pkgs.libxcrypt
+    pkgs.rustc
+    pkgs.libiconv
+    pkgs.cargo
+    pkgs.glibcLocales
+    pkgs.bash
+  ];
+}
